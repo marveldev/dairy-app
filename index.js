64621143e5083a966ebc8c9047bfc4561e26ccef
Marvel Dev <marvellousalika48@gmail.com>
2020-07-23 14:43:57 +0100

    -add string literals

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ let count = 1;
 
 function addEntryToDom(event) {
   event.preventDefault();
-
   const entryTextBoxValue = entryTextBox.value + '\n' + new Date().toLocaleString();
-
   const entryDiv = `
   <span class="single-entry">${entryTextBoxValue}</span>
   <button class="display-entry-button">${count}</button>
@@ -19,15 +17,19 @@ function addEntryToDom(event) {
   entryNav.innerHTML += entryDiv;
   entryTextBox.value = '';
   
-  const displayEntryButtons = document.querySelectorAll('.display-entry-button')
-  
-  displayEntryButtons.forEach(displayEntryButton => {
-    displayEntryButton.addEventListener('click', function displayDiary() {
-      displayEntryButton.previousElementSibling.style.display = 'block'
-    })
-  });
-  
   count++
+
+  const displayEntryButtons = document.querySelectorAll('.display-entry-button');
+  
+  displayEntryButtons.forEach( displayEntryButton => {
+    displayEntryButton.addEventListener('click', function () {
+      console.log("ok");
+      const allEntries = document.querySelectorAll('.single-entry');
+      for (let index = 0; index < allEntries.length; index++) {
+        allEntries[index].style.display = 'none';
+      }
+    });
+  });
 }
 
 entryForm.addEventListener('submit', addEntryToDom);
